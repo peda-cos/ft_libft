be4fd3e11a486deb2a305fd6306f0ef499ad20e7
Pedro Da Costa E Silva Monteiro <peda-cos@c2r5p5.42sp.org.br>
2024-10-04 17:57:27 -0300

    libft.h

---
+++ libft.h
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: peda-cos <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/04 12:06:47 by peda-cos          #+#    #+#             */
+/*   Updated: 2024/10/04 13:21:59 by peda-cos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <stdlib.h>
+
+int		ft_isalnum(int c);
+int		ft_isalpha(int c);
+int		ft_isascii(int c);
+int		ft_isdigit(int c);
+int		ft_isprint(int c);
+
+void	*ft_memcpy(void *dst, const void *src, size_t n);
+void	*ft_memmove(void *dst, const void *src, size_t len);
+void	*ft_memset(void *s, int c, size_t n);
+
+size_t		ft_strlcat(char *dst, const char *src, size_t dstsize);
+size_t		ft_strlcpy(char *dst, const char *src, size_t dstsize);
+size_t		ft_strlen(const char *s);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+
+int		ft_tolower(int c);
+int		ft_toupper(int c);
+
+#endif
