e7645876f53377a968437b666afcf8067fe4ccf4
Pedro Da Costa E Silva Monteiro <peda-cos@c2r5p5.42sp.org.br>
2024-10-05 12:14:38 -0300

    ft_bzero.c

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,8 +6,13 @@
 /*   By: peda-cos <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/05 10:33:52 by peda-cos          #+#    #+#             */
-/*   Updated: 2024/10/05 10:33:54 by peda-cos         ###   ########.fr       */
+/*   Updated: 2024/10/05 12:12:33 by peda-cos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+void	ft_bzero(void *s, size_t n)
+{
+	ft_memset(s, 0, n);
+}
