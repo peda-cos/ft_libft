69760d24b2b9c1fdb11678c96e904c4183f1bd26
Pedro Da Costa E Silva Monteiro <peda-cos@c2r5p5.42sp.org.br>
2024-10-05 18:32:53 -0300

    ft_strchr.c

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: peda-cos <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/05 10:34:48 by peda-cos          #+#    #+#             */
-/*   Updated: 2024/10/05 16:09:10 by peda-cos         ###   ########.fr       */
+/*   Updated: 2024/10/05 18:32:41 by peda-cos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,18 @@
 
 char	*ft_strchr(const char *s, int c)
 {
-	
+	char	ch;
+	size_t	i;
+
+	ch = (char)c;
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == ch)
+			return ((char *)&s[i]);
+		i++;
+	}
+	if (ch == '\0')
+		return ((char *)&s[i]);
+	return (NULL);
 }
