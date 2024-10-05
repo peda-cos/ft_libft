13c786a4fedc6f5845a8b1d125eac8b4617769e8
Pedro Da Costa E Silva Monteiro <peda-cos@c1r5p1.42sp.org.br>
2024-10-05 20:27:59 -0300

    ft_memchr.c

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,8 +6,26 @@
 /*   By: peda-cos <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/05 10:33:59 by peda-cos          #+#    #+#             */
-/*   Updated: 2024/10/05 10:34:01 by peda-cos         ###   ########.fr       */
+/*   Updated: 2024/10/05 20:27:49 by peda-cos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	size_t			i;
+	unsigned char	ch;
+	unsigned char	*ptr;
+
+	ch = (unsigned char)c;
+	ptr = (unsigned char *)s;
+	i = 0;
+	while (i < n)
+	{
+		if (ptr[i] == ch)
+			return ((void *)&ptr[i]);
+		i++;
+	}
+	return (NULL);
+}
