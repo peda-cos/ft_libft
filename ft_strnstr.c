1abbe0ccaa4f3e4fb556f0c2ecfd2150975de3e7
Pedro Da Costa E Silva Monteiro <peda-cos@c1r5p1.42sp.org.br>
2024-10-05 19:16:30 -0300

    ft_strnstr.c

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,8 +6,27 @@
 /*   By: peda-cos <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/05 10:34:59 by peda-cos          #+#    #+#             */
-/*   Updated: 2024/10/05 10:35:00 by peda-cos         ###   ########.fr       */
+/*   Updated: 2024/10/05 19:16:18 by peda-cos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	size_t	needle_len;
+
+	if (!*needle)
+		return ((char *)haystack);
+	needle_len = ft_strlen(needle);
+	if (needle_len > len)
+		return (NULL);
+	while (*haystack && len >= needle_len)
+	{
+		if (ft_strncmp(haystack, needle, needle_len) == 0)
+			return ((char *)haystack);
+		haystack++;
+		len--;
+	}
+	return (NULL);
+}
