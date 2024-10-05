da3dbed94889029b9e4d95369fa71dc391eb2364
Pedro Da Costa E Silva Monteiro <peda-cos@c1r5p1.42sp.org.br>
2024-10-05 20:40:26 -0300

    ft_memcmp.c

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,8 +6,26 @@
 /*   By: peda-cos <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/05 10:34:06 by peda-cos          #+#    #+#             */
-/*   Updated: 2024/10/05 10:34:08 by peda-cos         ###   ########.fr       */
+/*   Updated: 2024/10/05 20:29:53 by peda-cos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	size_t				i;
+	const unsigned char	*p1;
+	const unsigned char	*p2;
+
+	p1 = (const unsigned char *)s1;
+	p2 = (const unsigned char *)s2;
+	i = 0;
+	while (i < n)
+	{
+		if (p1[i] != p2[i])
+			return (p1[i] - p2[i]);
+		i++;
+	}
+	return (0);
+}
