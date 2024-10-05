0a1f2b29ed73ff7ab981bdfe2102c7d6d810d4da
Pedro Da Costa E Silva Monteiro <peda-cos@c1r5p1.42sp.org.br>
2024-10-05 19:20:45 -0300

    ft_calloc.c

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,8 +6,24 @@
 /*   By: peda-cos <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/05 10:36:18 by peda-cos          #+#    #+#             */
-/*   Updated: 2024/10/05 10:36:19 by peda-cos         ###   ########.fr       */
+/*   Updated: 2024/10/05 19:20:38 by peda-cos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+#include <stdlib.h>
 
+void	*ft_calloc(size_t count, size_t size)
+{
+	size_t	total_size;
+	void	*ptr;
+
+	if (size != 0 && count > 65535 / size)
+		return (NULL);
+	total_size = count * size;
+	ptr = malloc(total_size);
+	if (!ptr)
+		return (NULL);
+	ft_memset(ptr, 0, total_size);
+	return (ptr);
+}
