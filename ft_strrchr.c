a9a1a71a10e977a20542660fbe1ce1a428ff0733
Pedro Da Costa E Silva Monteiro <peda-cos@c1r5p1.42sp.org.br>
2024-10-05 19:14:37 -0300

    ft_strrchr.c

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,8 +6,28 @@
 /*   By: peda-cos <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/05 10:34:42 by peda-cos          #+#    #+#             */
-/*   Updated: 2024/10/05 10:34:43 by peda-cos         ###   ########.fr       */
+/*   Updated: 2024/10/05 19:14:27 by peda-cos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+char	*ft_strrchr(const char *s, int c)
+{
+	char	*last;
+	char	ch;
+	size_t	i;
+
+	ch = (char)c;
+	last = NULL;
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == ch)
+			last = (char *)&s[i];
+		i++;
+	}
+	if (ch == '\0')
+		return ((char *)&s[i]);
+	return (last);
+}
