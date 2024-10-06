6f4c66f50b3b058ebe69c4a33b305a72484c4ee3
Pedro Da Costa E Silva Monteiro <peda-cos@c2r5p5.42sp.org.br>
2024-10-06 12:29:08 -0300

    ft_strmapi.c

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: peda-cos <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/05 12:38:11 by peda-cos          #+#    #+#             */
-/*   Updated: 2024/10/05 22:45:01 by peda-cos         ###   ########.fr       */
+/*   Updated: 2024/10/06 12:28:30 by peda-cos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,7 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 	str = (char *)malloc(sizeof(char) * (len + 1));
 	if (!str)
 		return (NULL);
+	i = 0;
 	while (i < len)
 	{
 		str[i] = f(i, s[i]);
