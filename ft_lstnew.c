d5fb6218acfe2578d7edd0e5a10d85842c6e1522
Pedro Da Costa E Silva Monteiro <peda-cos@c1r7p3.42sp.org.br>
2024-10-06 19:54:15 -0300

    ft_lstnew.c

---
+++ ft_lstnew.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: peda-cos <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/06 19:01:28 by peda-cos          #+#    #+#             */
+/*   Updated: 2024/10/06 19:53:40 by peda-cos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdlib.h>
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*new_node;
+
+	new_node = malloc(sizeof(t_list));
+	new_node->content = content;
+	new_node->content = NULL;
+	return (content);
+}
