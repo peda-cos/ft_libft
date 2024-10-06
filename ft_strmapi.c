ea8fc9b76cacf858836aeb5390859c409635f04c
Pedro Da Costa E Silva Monteiro <peda-cos@c1r5p1.42sp.org.br>
2024-10-05 22:45:14 -0300

    ft_strmapi.c

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,8 +6,30 @@
 /*   By: peda-cos <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/05 12:38:11 by peda-cos          #+#    #+#             */
-/*   Updated: 2024/10/05 12:38:12 by peda-cos         ###   ########.fr       */
+/*   Updated: 2024/10/05 22:45:01 by peda-cos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+#include <stdlib.h>
 
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char	*str;
+	size_t	len;
+	size_t	i;
+
+	if (!s || !f)
+		return (NULL);
+	len = ft_strlen(s);
+	str = (char *)malloc(sizeof(char) * (len + 1));
+	if (!str)
+		return (NULL);
+	while (i < len)
+	{
+		str[i] = f(i, s[i]);
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
