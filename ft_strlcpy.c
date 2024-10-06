fc25d76715c6f31f8a90a24fdaa6f372966e56d1
Spuko <83892026+SpukoBR@users.noreply.github.com>
2024-10-06 08:36:35 -0300

    ft_strlcpy.c

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,24 +6,26 @@
 /*   By: peda-cos <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/04 07:07:20 by peda-cos          #+#    #+#             */
-/*   Updated: 2024/10/04 07:21:38 by peda-cos         ###   ########.fr       */
+/*   Updated: 2024/10/06 08:36:12 by peda-cos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
 	size_t	src_len;
+	size_t	i;
 
 	src_len = ft_strlen(src);
-	if (dstsize == 0)
+	if (size == 0)
 		return (src_len);
-	while (*src && dstsize > 1)
+	i = 0;
+	while (i < src_len && i < (size - 1))
 	{
-		*dst++ = *src++;
-		dstsize--;
+		dst[i] = src[i];
+		i++;
 	}
-	*dst = '\0';
+	dst[i] = '\0';
 	return (src_len);
 }
