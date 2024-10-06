f24c2ff077c089023cbf7a4d5056769b4309a8a3
Pedro Da Costa E Silva Monteiro <peda-cos@c1r5p1.42sp.org.br>
2024-10-05 22:10:32 -0300

    ft_subsstr.c

--- ft_substr.c
+++ ft_substr.c
@@ -6,8 +6,29 @@
 /*   By: peda-cos <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/05 12:38:16 by peda-cos          #+#    #+#             */
-/*   Updated: 2024/10/05 12:38:17 by peda-cos         ###   ########.fr       */
+/*   Updated: 2024/10/05 22:09:59 by peda-cos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+#include <stdlib.h>
 
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*substr;
+	size_t	s_len;
+	size_t	sub_len;
+
+	s_len = ft_strlen(s);
+	if (start >= s_len)
+		return (ft_strdup(""));
+	if (s_len - start < len)
+		sub_len = s_len - start;
+	else
+		sub_len = len;
+	substr = (char *)malloc(sizeof(char) * (sub_len + 1));
+	if (!substr)
+		return (NULL);
+	ft_strlcpy(substr, s + start, sub_len + 1);
+	return (substr);
+}
