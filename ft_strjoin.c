f6dbe4b67d9a6dcf2f90c69358ab47ee416737a0
Pedro Da Costa E Silva Monteiro <peda-cos@c1r5p1.42sp.org.br>
2024-10-05 22:19:58 -0300

    ft_strjoin.c

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,8 +6,26 @@
 /*   By: peda-cos <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/05 12:38:22 by peda-cos          #+#    #+#             */
-/*   Updated: 2024/10/05 12:38:23 by peda-cos         ###   ########.fr       */
+/*   Updated: 2024/10/05 22:19:42 by peda-cos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+#include <stdlib.h>
 
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	len1;
+	size_t	len2;
+	char	*joined;
+
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
+	joined = (char *)malloc(sizeof(char) * (len1 + len2 + 1));
+	if (!joined)
+		return (NULL);
+	ft_memcpy(joined, s1, len1);
+	ft_memcpy(joined + len1, s2, len2);
+	joined[len1 + len2] = '\0';
+	return (joined);
+}
