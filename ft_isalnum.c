e47c725cad8b06fb2f64af8d0cc1fbd6422547e2
Spuko <83892026+SpukoBR@users.noreply.github.com>
2024-10-06 08:29:28 -0300

    ft_isalnum.c

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -6,7 +6,7 @@
 /*   By: peda-cos <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/04 06:04:45 by peda-cos          #+#    #+#             */
-/*   Updated: 2024/10/04 06:04:59 by peda-cos         ###   ########.fr       */
+/*   Updated: 2024/10/06 08:29:24 by peda-cos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,5 @@
 
 int	ft_isalnum(int c)
 {
-	if (ft_isalpha(c) || ft_isdigit(c))
-		return (1);
-	return (0);
+	return (ft_isalpha(c) || ft_isdigit(c));
 }
