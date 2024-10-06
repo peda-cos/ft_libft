95839665bf3dec44aa7f30a68d175b7a89cb1261
Pedro Da Costa E Silva Monteiro <peda-cos@c1r5p1.42sp.org.br>
2024-10-05 22:29:15 -0300

    ft_strtrim.c

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,8 +6,30 @@
 /*   By: peda-cos <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/05 12:38:27 by peda-cos          #+#    #+#             */
-/*   Updated: 2024/10/05 12:38:29 by peda-cos         ###   ########.fr       */
+/*   Updated: 2024/10/05 22:28:45 by peda-cos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+#include <stdlib.h>
 
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	size_t	start;
+	size_t	end;
+	size_t	trim_len;
+	char	*trimmed;
+
+	start = 0;
+	while (s1[start] && ft_strchr(set, s1[start]))
+		start++;
+	end = ft_strlen(s1);
+	while (end > start && ft_strchr(set, s1[end -1]))
+		end--;
+	trim_len = end - start;
+	trimmed = (char *)malloc(sizeof(char) * (trim_len + 1));
+	if (!trimmed)
+		return (NULL);
+	ft_strlcpy(trimmed, s1 + start, trim_len + 1);
+	return (trimmed);
+}
