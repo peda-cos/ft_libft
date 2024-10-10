10f6f36ffe0b9e65252c8a2ffeac58f88ddfc409
Spuko <83892026+SpukoBR@users.noreply.github.com>
2024-10-10 04:36:08 -0300

    ft_strdup.c

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,22 +6,22 @@
 /*   By: peda-cos <peda-cos@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/05 10:36:24 by peda-cos          #+#    #+#             */
-/*   Updated: 2024/10/09 07:05:53 by peda-cos         ###   ########.fr       */
+/*   Updated: 2024/10/10 04:36:04 by peda-cos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdlib.h>
 
-char	*ft_strdup(const char *s1)
+char	*ft_strdup(const char *s)
 {
 	size_t	len;
 	char	*dup;
 
-	len = ft_strlen(s1) + 1;
+	len = ft_strlen(s) + 1;
 	dup = (char *)malloc(sizeof(char) * len);
 	if (!dup)
 		return (NULL);
-	ft_strlcpy(dup, s1, len);
+	ft_strlcpy(dup, s, len);
 	return (dup);
 }
