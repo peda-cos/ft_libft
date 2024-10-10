252d43ab994165e5c89f049e5a7bf087e10d9ac0
Spuko <83892026+SpukoBR@users.noreply.github.com>
2024-10-10 04:35:14 -0300

    ft_calloc.c

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,19 +6,19 @@
 /*   By: peda-cos <peda-cos@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/05 10:36:18 by peda-cos          #+#    #+#             */
-/*   Updated: 2024/10/10 04:34:32 by peda-cos         ###   ########.fr       */
+/*   Updated: 2024/10/10 04:35:10 by peda-cos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdlib.h>
 
-void	*ft_calloc(size_t nmem, size_t size)
+void	*ft_calloc(size_t nmemb, size_t size)
 {
 	size_t	total_size;
 	void	*ptr;
 
-	total_size = nmem * size;
+	total_size = nmemb * size;
 	ptr = malloc(total_size);
 	if (!ptr)
 		return (NULL);
