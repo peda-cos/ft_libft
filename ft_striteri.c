739234a5f235db1bb398cca04753b3c634d20569
Spuko <83892026+SpukoBR@users.noreply.github.com>
2024-10-15 10:01:57 -0300

    no.h

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,18 +6,16 @@
 /*   By: peda-cos <peda-cos@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/05 12:38:45 by peda-cos          #+#    #+#             */
-/*   Updated: 2024/10/07 15:59:13 by peda-cos         ###   ########.fr       */
+/*   Updated: 2024/10/15 10:01:41 by peda-cos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include "libft.h"
 
 void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 {
 	unsigned int	i;
 
 	if (!s || !f)
-		return ;
+		return (0);
 	i = 0;
 	while (s[i])
 	{
