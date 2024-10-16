bb4ae8e4caf7d8ec35d6bfd21658226609c7f982
Pedro Monteiro <83892026+peda-cos@users.noreply.github.com>
2024-10-16 10:58:50 -0300

    Update libft.h

--- libft.h
+++ libft.h
@@ -44,7 +44,7 @@ char				*ft_strdup(const char *s1);
 char				*ft_strchr(const char *s, int c);
 char				*ft_strrchr(const char *s, int c);
 int					ft_strncmp(const char *s1, const char *s2, size_t n);
-char				*ft_strnstr(const char *haystack, const char *needle,
+char				*ft_strnstr(const char *big, const char *little,
 						size_t len);
 char				*ft_substr(char const *s, unsigned int start, size_t len);
 char				*ft_strjoin(char const *s1, char const *s2);
