ca7ce0f039ecbcdb13c6bbed750e27446b6a77d0
Spuko <83892026+SpukoBR@users.noreply.github.com>
2024-10-19 11:21:22 -0300

    fix: correção das normas com base nas normas da 42

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -14,7 +14,7 @@
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	size_t			i;
+	size_t				i;
 	const unsigned char	*p1;
 	const unsigned char	*p2;
 
--- ft_memcpy.c
+++ ft_memcpy.c
@@ -14,8 +14,8 @@
 
 void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
-	size_t			i;
-	unsigned char	*d;
+	size_t				i;
+	unsigned char		*d;
 	const unsigned char	*s;
 
 	d = (unsigned char *)dest;
