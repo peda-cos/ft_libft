8025899456b9af727060d97ff0a858e0b9bd3e63
Pedro da Costa e Silva Monteiro <83892026+peda-cos@users.noreply.github.com>
2025-01-31 07:01:57 -0300

    refactor: refactor ft_atoi to use ft_strtol for integer conversion

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: peda-cos <peda-cos@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/05 10:33:46 by peda-cos          #+#    #+#             */
-/*   Updated: 2024/10/10 04:33:01 by peda-cos         ###   ########.fr       */
+/*   Updated: 2025/01/31 06:48:26 by peda-cos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,5 @@
 
 int	ft_atoi(const char *nptr)
 {
-	int	sign;
-	int	result;
-
-	if (nptr == NULL)
-		return (0);
-	sign = 1;
-	result = 0;
-	while ((*nptr >= 9 && *nptr <= 13) || *nptr == ' ')
-		nptr++;
-	if (*nptr == '-' || *nptr == '+')
-	{
-		if (*nptr == '-')
-			sign = -1;
-		nptr++;
-	}
-	while (ft_isdigit(*nptr))
-	{
-		result = result * 10 + (*nptr - '0');
-		nptr++;
-	}
-	return (sign * result);
+	return ((int)ft_strtol(nptr, (char **)NULL, 10));
 }
