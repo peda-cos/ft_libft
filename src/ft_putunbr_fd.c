bca616932a4f4d17939c167e9bc2ef7ee11ebccb
Pedro da Costa e Silva Monteiro <83892026+peda-cos@users.noreply.github.com>
2025-05-17 22:33:46 -0300

    feat: add standard unsigned integer output function

--- src/ft_putunbr_fd.c
+++ src/ft_putunbr_fd.c
@@ -6,11 +6,18 @@
 /*   By: peda-cos <peda-cos@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 09:04:45 by peda-cos          #+#    #+#             */
-/*   Updated: 2025/05/16 17:17:25 by peda-cos         ###   ########.fr       */
+/*   Updated: 2025/05/17 22:18:04 by peda-cos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+void	ft_putunbr_fd(unsigned int n, int fd)
+{
+	if (n >= 10)
+		ft_putunbr_fd(n / 10, fd);
+	ft_putchar_fd((n % 10) + '0', fd);
+}
 
 int	ft_printf_putunbr_fd(unsigned int n, int fd)
 {
