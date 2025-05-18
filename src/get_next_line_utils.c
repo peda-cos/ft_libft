164060c7fc43b67aa164a640f4b314a2f541c13d
Pedro da Costa e Silva Monteiro <83892026+peda-cos@users.noreply.github.com>
2025-05-17 22:33:46 -0300

    refactor: consolidate utility functions in get_next_line_utils.c

--- src/get_next_line_utils.c
+++ src/get_next_line_utils.c
@@ -6,96 +6,34 @@
 /*   By: peda-cos <peda-cos@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 03:49:23 by peda-cos          #+#    #+#             */
-/*   Updated: 2024/11/13 17:39:19 by peda-cos         ###   ########.fr       */
+/*   Updated: 2025/05/17 22:23:57 by peda-cos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlen(const char *str)
+char	*handle_read_error(char *storage)
 {
-	int	len;
-
-	len = 0;
-	while (str[len])
-		len++;
-	return (len);
-}
-
-char	*ft_strchr(const char *s, int c)
-{
-	while (*s)
-	{
-		if (*s == (char)c)
-			return ((char *)s);
-		s++;
-	}
-	if (c == '\0')
-		return ((char *)s);
+	free(storage);
 	return (NULL);
 }
 
-char	*ft_strdup(const char *s1)
+char	*init_storage(void)
 {
-	char	*copy;
-	int		i;
+	char	*storage;
 
-	copy = (char *)malloc(sizeof(char) * (ft_strlen(s1) + 1));
-	if (!copy)
+	storage = ft_strdup("");
+	if (!storage)
 		return (NULL);
-	i = 0;
-	while (s1[i])
-	{
-		copy[i] = s1[i];
-		i++;
-	}
-	copy[i] = '\0';
-	return (copy);
+	return (storage);
 }
 
-char	*ft_strjoin(char const *s1, char const *s2)
+char	*append_buffer(char *storage, char *buffer)
 {
-	char	*joined;
-	int		i;
-	int		j;
+	char	*temp;
 
-	joined = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
-	if (!joined)
-		return (NULL);
-	i = 0;
-	while (s1[i])
-	{
-		joined[i] = s1[i];
-		i++;
-	}
-	j = 0;
-	while (s2[j])
-	{
-		joined[i + j] = s2[j];
-		j++;
-	}
-	joined[i + j] = '\0';
-	return (joined);
-}
-
-char	*ft_substr(char const *s, unsigned int start, size_t len)
-{
-	char	*sub;
-	size_t	i;
-
-	if (!s)
-		return (NULL);
-	if ((size_t)start >= ft_strlen(s))
-		return (ft_strdup(""));
-	sub = (char *)malloc(sizeof(char) * (len + 1));
-	if (!sub)
-		return (NULL);
-	i = 0;
-	while (s[start + i] && i < len)
-	{
-		sub[i] = s[start + i];
-		i++;
-	}
-	sub[i] = '\0';
-	return (sub);
+	temp = storage;
+	storage = ft_strjoin(temp, buffer);
+	free(temp);
+	return (storage);
 }
