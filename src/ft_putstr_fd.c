89f1f93a7e0e879fd03b04f698f44a56101b9436
Pedro da Costa e Silva Monteiro <83892026+peda-cos@users.noreply.github.com>
2025-05-17 22:33:46 -0300

    feat: implement standard string output function

--- src/ft_putstr_fd.c
+++ src/ft_putstr_fd.c
@@ -6,11 +6,22 @@
 /*   By: peda-cos <peda-cos@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/05 12:29:03 by peda-cos          #+#    #+#             */
-/*   Updated: 2025/05/16 17:18:49 by peda-cos         ###   ########.fr       */
+/*   Updated: 2025/05/17 22:18:04 by peda-cos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+void	ft_putstr_fd(char *s, int fd)
+{
+	if (!s)
+		return ;
+	while (*s)
+	{
+		ft_putchar_fd(*s, fd);
+		s++;
+	}
+}
 
 int	ft_printf_putstr_fd(char *s, int fd)
 {
