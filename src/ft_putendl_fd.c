d47a2b2cb637b98e4d92bed5246d84d04f9a363d
Pedro da Costa e Silva Monteiro <83892026+peda-cos@users.noreply.github.com>
2025-05-17 22:33:46 -0300

    feat: implement standard and printf-compatible putendl_fd functions

--- src/ft_putendl_fd.c
+++ src/ft_putendl_fd.c
@@ -6,16 +6,27 @@
 /*   By: peda-cos <peda-cos@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/05 12:32:15 by peda-cos          #+#    #+#             */
-/*   Updated: 2025/05/16 17:16:16 by peda-cos         ###   ########.fr       */
+/*   Updated: 2025/05/17 22:30:13 by peda-cos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_printf_putendl_fd(char *s, int fd)
+void	ft_putendl_fd(char *s, int fd)
 {
 	if (!s)
 		return ;
-	ft_printf_putstr_fd(s, fd);
-	ft_printf_putchar_fd('\n', fd);
+	ft_putstr_fd(s, fd);
+	ft_putchar_fd('\n', fd);
+}
+
+int	ft_printf_putendl_fd(char *s, int fd)
+{
+	int	count;
+
+	if (!s)
+		return (0);
+	count = ft_printf_putstr_fd(s, fd);
+	count += ft_printf_putchar_fd('\n', fd);
+	return (count);
 }
