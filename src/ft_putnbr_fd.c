4fe2a3beadea48eec10f112e5ce82e51e9606836
Pedro da Costa e Silva Monteiro <83892026+peda-cos@users.noreply.github.com>
2025-05-17 22:33:46 -0300

    feat: implement standard integer output function

--- src/ft_putnbr_fd.c
+++ src/ft_putnbr_fd.c
@@ -6,11 +6,26 @@
 /*   By: peda-cos <peda-cos@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/05 12:38:53 by peda-cos          #+#    #+#             */
-/*   Updated: 2025/05/16 17:19:10 by peda-cos         ###   ########.fr       */
+/*   Updated: 2025/05/17 22:18:04 by peda-cos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	long	num;
+
+	num = n;
+	if (num < 0)
+	{
+		ft_putchar_fd('-', fd);
+		num = -num;
+	}
+	if (num >= 10)
+		ft_putnbr_fd(num / 10, fd);
+	ft_putchar_fd((num % 10) + '0', fd);
+}
 
 int	ft_printf_putnbr_fd(int n, int fd)
 {
