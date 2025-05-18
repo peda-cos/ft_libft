ef4355142ff8fd536cb2bac14bab1d5a484fdbb2
Pedro da Costa e Silva Monteiro <83892026+peda-cos@users.noreply.github.com>
2025-05-17 22:33:46 -0300

    feat: add standard hex printing function alongside printf variant

--- src/ft_puthex_fd.c
+++ src/ft_puthex_fd.c
@@ -6,11 +6,24 @@
 /*   By: peda-cos <peda-cos@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 09:04:45 by peda-cos          #+#    #+#             */
-/*   Updated: 2025/05/16 17:18:38 by peda-cos         ###   ########.fr       */
+/*   Updated: 2025/05/17 22:18:04 by peda-cos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+void	ft_puthex_fd(unsigned int n, int uppercase, int fd)
+{
+	char	*base;
+
+	if (uppercase)
+		base = "0123456789ABCDEF";
+	else
+		base = "0123456789abcdef";
+	if (n >= 16)
+		ft_puthex_fd(n / 16, uppercase, fd);
+	ft_putchar_fd(base[n % 16], fd);
+}
 
 static int	ft_puthex(unsigned int n, const char *base, int fd)
 {
