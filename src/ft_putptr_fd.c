75b0d4b464c9b95fd7126aa68abe046ec8f6e011
Pedro da Costa e Silva Monteiro <83892026+peda-cos@users.noreply.github.com>
2025-05-17 22:33:46 -0300

    feat: add standard pointer printing function

--- src/ft_putptr_fd.c
+++ src/ft_putptr_fd.c
@@ -6,11 +6,21 @@
 /*   By: peda-cos <peda-cos@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 09:04:45 by peda-cos          #+#    #+#             */
-/*   Updated: 2025/05/16 17:18:14 by peda-cos         ###   ########.fr       */
+/*   Updated: 2025/05/17 22:18:04 by peda-cos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+void	ft_putptr_fd(void *ptr, int fd)
+{
+	unsigned long	addr;
+
+	addr = (unsigned long)ptr;
+	if (addr >= 16)
+		ft_putptr_fd((void *)(addr / 16), fd);
+	ft_putchar_fd("0123456789abcdef"[addr % 16], fd);
+}
 
 int	ft_printf_putptr_fd(void *ptr, int fd)
 {
